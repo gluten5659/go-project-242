8e44ea806fea6f03c75a99f9c51d25e5ac0be5e1
gluten5659 <140404099+gluten5659@users.noreply.github.com>
2026-04-30 17:46:33 +0500

    wip

--- cmd/hexlet-path-size/main.go
+++ cmd/hexlet-path-size/main.go
@@ -2,11 +2,58 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"log"
 	"os"
 
 	"github.com/urfave/cli/v3"
 )
 
 func main() {
-	(&cli.Command{}).Run(context.Background(), os.Args)
+	cmd := &cli.Command{
+		Action: func(ctx context.Context, cmd *cli.Command) error {
+			fmt.Printf("Hello %q", cmd.Args().Get(0))
+			GetSize(path string)
+
+			return nil
+		},
+	}
+	if err := cmd.Run(context.Background(), os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func GetSize(path string) (int, error) {
+	size, err := getFileSize(path)
+	if err != nil {
+		size, err = getFolderSize(path)
+	}
+	return size, err
+}
+
+func getFileSize(filePath string) (int, error) {
+	stat, err := os.Lstat(filePath)
+	if err != nil {
+		return 0, err
+	}
+	return int(stat.Size()), nil
+}
+
+func getFolderSize(folderPath string) (int, error) {
+	files, err := os.ReadDir(folderPath)
+	if err != nil {
+		return 0, err
+	}
+	folderSize := 0
+	for _, file := range files {
+		if file.Type().IsDir() {
+			continue
+		}
+		fileInfo, err := file.Info()
+		if err != nil {
+			return 0, err
+		}
+		folderSize += int(fileInfo.Size())
+	}
+	return folderSize, nil
 }
